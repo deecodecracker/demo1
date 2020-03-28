fbe1dc6a4eebb81d56305e655bb9a94b86e1a9b7
deecodecracker <deepak.j.a.30@gmail.com>
2020-03-29 03:34:21 +0530

    initial commit to feature-newbranch2

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -5,10 +5,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Start of program");
         int sum = add(5,8);
-
+        double difference = difference(80d,50d);
         System.out.println("The sum is : " + sum);
-
+        System.out.println("The difference is : " + difference);
         System.out.println("End of program");
+    }
+
+    private static double difference(double a, double b) {
+        return a-b;
     }
 
     private static int add(int a,int b) {
